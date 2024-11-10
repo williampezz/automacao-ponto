fe148d0027acaf1c2cb6552dd20c25cf32268e2f
william <williamaugustopez@gmail.com>
2024-11-10 12:32:56 -0300

    adicionando apontamentos

--- cypress/support/apontamentoPage/ApontamentoPage.js
+++ cypress/support/apontamentoPage/ApontamentoPage.js
@@ -10,10 +10,31 @@ class Apontamentos {
         cy.get(elem.Alocacao).select('SOMA MATRIZ - GROWDEV SERVICOS TECNOLOGIA DA INFORMACAO LTDA');
         cy.get(elem.BtAbrir).click()
 
-        
-        
-        
+    }
 
+    adicionarHorariosTrabalhados() {
+        cy.get(elem.entrada0110).type('08:00')
+        cy.get(elem.almoco0110).type('12:00')
+        cy.get(elem.voltaalmoco0110).type('13:00')
+        cy.get(elem.saida0110).type('18:00')
+
+
+        cy.get(elem.entrada0210).type('08:00')
+        cy.get(elem.almoco0210).type('12:00')
+        cy.get(elem.voltaalmoco0210).type('13:00')
+        cy.get(elem.saida0210).type('18:00')
+
+        cy.get(elem.entrada0310).type('08:00')
+        cy.get(elem.almoco0310).type('12:00')
+        cy.get(elem.voltaalmoco0310).type('13:00')
+        cy.get(elem.saida0310).type('18:00')
+
+        cy.get(elem.entrada0410).type('08:00')
+        cy.get(elem.almoco0410).type('12:00')
+        cy.get(elem.voltaalmoco0410).type('13:00')
+        cy.get(elem.saida0410).type('18:00')
+
+        
     }
 
 }
--- cypress/support/page/LoginPage.js
+++ cypress/support/page/LoginPage.js
@@ -6,7 +6,6 @@ class Login {
     loginSuccess() {
         cy.get(elem.inputCPF).type('04424886026')
         cy.get(elem.inputCPF).type(Cypress.env('CPF_TESTE'));
-
         cy.get(elem.inputPassword).type('04112000')
         cy.get(elem.btnLogin).click()
         cy.get(elem.imgApontamento).click()
